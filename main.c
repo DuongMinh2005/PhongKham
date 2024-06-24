70c5aa3c56ca1f45fac614e793372505f089f380
DuongMinh2005 <tenminh0402@gmail.com>
2024-06-24 09:15:44 +0700

    Update main.c

--- main.c
+++ main.c
@@ -7,53 +7,20 @@ int main(){
     BenhNhan benhnhan;
     BacSi bacsi;
     KhoDuoc khoduoc;
-    
     {
 menu:
         int a;
         printf(".______________________________.\n");
         printf("|             MENU             |\n");
         printf(".______________________________.\n");
-        printf("1.QUAN LY TAI KHOAN\n");
-        printf("2.QUAN LY BENH NHAN\n");
-        printf("3.QUAN LY NHA SI\n");
-        printf("4.QUAN LY KHO DUOC\n");
-        printf("5.DANG XUAT\n");
+        printf("1.QUAN LY BENH NHAN\n");
+        printf("2.QUAN LY NHA SI\n");
+        printf("3.QUAN LY KHO DUOC\n");
+        printf("4.DANG XUAT\n");
         printf("VUI LONG CHON CHUC NANG: ");
         scanf("%d",&a);
         switch (a) {
             case 1:
-quanlitaikhoan:
-                printf(".______________________________.\n");
-                printf("|       QUAN LY TAI KHOAN      |\n");
-                printf(".______________________________.\n");
-                printf("1.TAO TAI KHOAN\n");
-                printf("2.XOA TAI KHOAN\n");
-                printf("3.SUA TAI KHOAN\n");
-                printf("4.QUAY LAI\n");
-                printf("VUI LONG CHON CHUC NANG: ");
-                int d;
-                scanf("%d",&d);
-                switch (d) {
-                    case 1:
-                        TaoTK();
-                        break;
-                    case 2:
-                        xoaTK();
-                        break;
-                    case 3:
-                        suaTK();
-                        break;
-                    case 4:
-                        goto menu;
-                    default:
-                        printf("!!!CHUC NANG KHONG PHU HOP!!!\n");
-                        printf("======VUI LONG CHON LAI======\n\n");
-                        goto quanlitaikhoan;
-                        break;
-                }
-                goto quanlitaikhoan;
-            case 2:
 quanlibenhnhan:
                 printf(".______________________________.\n");
                 printf("|       QUAN LY BENH NHAN      |\n");
@@ -69,7 +36,7 @@ quanlibenhnhan:
                 scanf("%d",&b);
                 switch (b){
                     case 1:
-                        nhapThongTinBenhNhan();
+                        nhapThongTinBenhNhan(&benhnhan);
                         break;
                     case 2:
                         xoaThongTinBenhNhan();
@@ -92,7 +59,7 @@ quanlibenhnhan:
                         break;
                 }
                 goto quanlibenhnhan;
-            case 3:
+            case 2:
 quanlibacsi:
                 printf(".______________________________.\n");
                 printf("|        QUAN LY NHA SI        |\n");
@@ -108,7 +75,7 @@ quanlibacsi:
                 scanf("%d",&c);
                 switch (c){
                     case 1:
-                        nhapThongTinBacSi();
+                        nhapThongTinBacSi(&bacsi);
                         break;
                     case 2:
                         xoaThongTinBacSi();
@@ -131,7 +98,7 @@ quanlibacsi:
                         goto quanlibacsi;
                 }
                 goto quanlibacsi;
-            case 4:
+            case 3:
 quanlikhoduoc:
                 printf(".______________________________.\n");
                 printf("|        QUAN LY KHO DUOC      |\n");
@@ -166,19 +133,19 @@ quanlikhoduoc:
                         break;
                     default:
                         printf("!!!CHUC NANG KHONG PHU HOP!!!\n");
-                        printf("======VUI LONG CHON LAI======\n");
+                        printf("======VUI LONG CHON LAI======\n\n");
                         goto quanlikhoduoc;
                         break;
                 }
                 goto quanlikhoduoc;
                 break;
-            case 5:
+            case 4:
                 printf("____DANG XUAT THANH CONG____\n");
-                goto tryagain;
+                return 0;
             default:
                 printf("!!!CHUC NANG KHONG PHU HOP!!!\n");
+                printf("======VUI LONG CHON LAI======\n\n");
                 goto menu;
-                break;
         }
     }
     return 0;
