f4a14c75a4812c405a199ad924aa63136aae0ff6
TMelight <chamanhelight2005@gmail.com>
2024-06-24 10:36:42 +0700

    Update main.c

--- main.c
+++ main.c
@@ -377,12 +377,12 @@ void timThongTinThuoc(){
     fgets(masothuoc,sizeof(masothuoc),stdin);
     snprintf(search_line, sizeof(search_line),"Ma so thuoc: %s",masothuoc);
 
-    while(fgets(line,sizeof(line),stdin)){
+    while(fgets(line,sizeof(line),file)){
         if(strcmp(line, search_line)==0){
             found =1;
             printf("%s", line);
             for ( int i =0 ; i<8;++i){
-                if(fgets(line,sizeof(line),stdin))
+                if(fgets(line,sizeof(line),file))
                     printf("%s", line);
             }
             break;
